ca26357d432c4b8d8a785fb1c8fe8ca1574b0043
Maneedhar <maneedhar@ideatostartup.org>
2021-12-25 23:40:07 +0530

    auth middleware

---
+++ pages/_middleware.js
@@ -0,0 +1,23 @@
+import { getToken } from 'next-auth/jwt';
+import { NextResponse } from 'next/server';
+
+export async function middleware(req) {
+  // token will exist if user is logged in
+  const token = await getToken({ req, secret: process.env.JWT_SECRET });
+
+  const { pathname } = req.nextUrl;
+
+  if (token && pathname === '/login') return NextResponse.redirect('/');
+
+  // allow requests if
+  // 1. token exists
+  // 2. if it's a request for next-auth session
+  if (pathname.includes('/api/auth') || token) {
+    return NextResponse.next();
+  }
+
+  // redirect to login if there's no token and are requesting a protected route
+  if (!token && pathname !== '/login') {
+    return NextResponse.redirect('/login');
+  }
+}
