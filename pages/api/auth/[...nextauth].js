759d05bc1c04766a416e65a3a5e727509ab03b82
Maneedhar <maneedhar@ideatostartup.org>
2021-12-25 22:30:24 +0530

    login with spotify

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,76 @@
+import NextAuth from 'next-auth';
+import SpotifyProvider from 'next-auth/providers/spotify';
+import spotifyApi, { LOGIN_URL } from '../../../lib/spotify';
+
+const refreshAccessToken = async (token) => {
+  try {
+    spotifyApi.setAccessToken(token.accessToken);
+    spotifyApi.setRefreshToken(token.refreshToken);
+
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
+    console.log('Refreshed token:', refreshedToken);
+
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpiresAt: Date.now() + refreshedToken.expires_in * 1000, // 1 hour from now in ms as spotify returns expires_in in seconds (3600)
+      // refresh if there is new token else fallback to old token
+      refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      ...token,
+      error: 'refresh access token error',
+    };
+  }
+};
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    SpotifyProvider({
+      clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+      authorization: LOGIN_URL,
+    }),
+    // ...add more providers here
+  ],
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    async jwt({ account, token, user }) {
+      // initial sign in
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccountId,
+          accessTokenExpiresAt: account.expires_at * 1000, // from seconds to milliseconds
+        };
+      }
+
+      // return previous token if the access token has not expired yet
+      if (Date.now() < token.accessTokenExpiresAt) {
+        console.log('token valid');
+        return token;
+      }
+
+      // refresh token
+      console.log('refreshing token');
+      return await refreshAccessToken(token);
+    },
+
+    async session({ session, token }) {
+      session.user.accessToken = token.accessToken;
+      session.user.refreshToken = token.refreshToken;
+      session.user.username = token.username;
+
+      return session;
+    },
+  },
+});
